56ae3840d9843598d079692924fb3acfba36a44a
vanessa <vanessa@Vanessa>
2016-10-11 10:11:05 +0200

    Sistemata grafica e logica attacco, rinominati a/b in pg1/pg2

--- src/it/dstech/model/CombatManager.java
+++ src/it/dstech/model/CombatManager.java
@@ -4,30 +4,38 @@ public class CombatManager {
 	public void combatti(IPersonaggio a, IPersonaggio b) {
 		int dannoA = a.mossa() - b.getDifesaPersonaggio();
 		int dannoB = b.mossa() - a.getDifesaPersonaggio();
-		int hpA=a.getHpPersonaggio();
-		int hpB=b.getHpPersonaggio();
-		do{
-			hpB = hpB - dannoA;
-			System.out.println("" + a.getNomePersonaggio()+" - "+hpA);
-			System.out.println("" + b.getNomePersonaggio()+" - "+hpB);
-			if(b.getHpPersonaggio()<=0){
-				System.out.println(b.getNomePersonaggio()+" è morto.");
-				System.exit(0);
-			}			
-			hpA=(hpA - dannoB);
-			System.out.println("" + b.getNomePersonaggio()+" - "+hpB);
-			System.out.println("" + a.getNomePersonaggio()+" - "+hpA);
-			if(a.getHpPersonaggio()<=0){
-				System.out.println(a.getNomePersonaggio()+" è morto.");
-				System.exit(0);
+		int hpA = a.getHpPersonaggio();
+		int hpB = b.getHpPersonaggio();
+		do {
+
+			if (hpA <= 0) {
+				System.out.println(a.getNomePersonaggio() + " è morto.");
+			} else {
+
+				hpB = hpB - dannoA;
+				System.out.println("-------------------------------");
+				System.out.println("ATTACCA " + a.getNomePersonaggio() + " - " + hpA);
+				System.out.println("SUBISCE " + b.getNomePersonaggio() + " - " + hpB);
+
+				if (hpB <= 0) {
+					System.out.println(b.getNomePersonaggio() + " è morto.");
+				} else {
+
+					hpA = (hpA - dannoB);
+					System.out.println("-------------------------------");
+					System.out.println("ATTACCA " + b.getNomePersonaggio() + " - " + hpB);
+					System.out.println("SUBISCE " + a.getNomePersonaggio() + " - " + hpA);
+
+				}
 			}
-		} while ((hpB > 0) && (hpA>0));
+
+		} while ((hpB > 0) && (hpA > 0));
 	}
 
 	public void stampaInfo(IPersonaggio a, IPersonaggio b) {
-		System.out.println("" + a.getNomePersonaggio());
-		System.out.println("" + a.getHpPersonaggio());
+		System.out.print("" + a.getNomePersonaggio() + " VS ");
 		System.out.println("" + b.getNomePersonaggio());
+		System.out.print("" + a.getHpPersonaggio() + "\t");
 		System.out.println("" + b.getHpPersonaggio());
 	}
 
